4ed9fbb53595185e81edcbec7ea7ea1a669d235c
pip-pipo <codemi692@gmail>
2021-02-04 13:39:31 -1100

    :fire: footer aded

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,6 +3,7 @@ import Main from '../MainHome/Main';
 import Navbar from '../Navabar/Navbar'
 import About from '../pages/About/About';
 import Contact from '../pages/Contact/Contact';
+import Footer from '../pages/Footer/Footer';
 import Services from '../pages/Services/Services';
 import Sidebar from '../Sidebar/Sidebar'
 
@@ -21,6 +22,7 @@ const Home = () => {
             <About/>
             <Services/>
             <Contact/>
+            <Footer/>
         </>
     )
 }
---
+++ src/Components/pages/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.scss'
+import { GoLocation } from "react-icons/go";
+import { AiOutlineMail ,AiOutlineCopyrightCircle} from "react-icons/ai";
+const Footer = () => {
+    return (
+        <div className="main_footer">
+        <div className="footer">
+            <div className="footer__first"><img src="https://avatars.githubusercontent.com/u/69667157?s=460&u=b3c95f91be2b4fdbd1ea91cc18ec796fd64268af&v=4" alt="my__img" />
+                <h4>Md Morsalin</h4>
+                <p>🌱 I’m currently learning everything 😉</p>
+            </div>
+            <div className="footer__middle">
+                <h4> <GoLocation />Location  :  Keranigonj Dhaka Bangladesh</h4>
+                <h4><AiOutlineMail/> Email:Codemi692@gmail.com</h4>
+            </div>
+            <div className="footer__last">
+            </div>
+
+        </div>
+            <div className="footer__copy__right">
+             <AiOutlineCopyrightCircle/>   CopyRight Terms under 2021
+            </div>
+        </div>
+    )
+}
+
+export default Footer
